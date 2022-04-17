88e07c45e18d2a6ff06c22c15c9dbaeeff4b701b
Bhushan Asutkar <bhushanasutkar321@gmail.com>
2022-04-17 21:53:45 +0530

    Updates

---
+++ precache-manifest.67cca2534e24152c74af60206bc6757c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478590e32a1c2538fdd700478ea1b65c",
+    "url": "/projects/react-hangman/index.html"
+  },
+  {
+    "revision": "6b2f10822bc1ec6f126b",
+    "url": "/projects/react-hangman/static/js/2.df82bfc7.chunk.js"
+  },
+  {
+    "revision": "cefa5548c3b3c6c56fcd",
+    "url": "/projects/react-hangman/static/js/main.93258834.chunk.js"
+  },
+  {
+    "revision": "3cee6bb24931a2a30c92",
+    "url": "/projects/react-hangman/static/js/runtime~main.6b60cba0.js"
+  }
+]);
